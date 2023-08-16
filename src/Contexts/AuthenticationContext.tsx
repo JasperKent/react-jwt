19739dc054b57269f360e0fdd12811eb19ed4635
Jasper Kent <software@jasperkent.com>
2023-08-16 11:02:20 +0100

    After video

--- src/Contexts/AuthenticationContext.tsx
+++ src/Contexts/AuthenticationContext.tsx
@@ -1,5 +1,8 @@
-import { createContext, PropsWithChildren, useState } from "react";
+import { createContext, PropsWithChildren, useEffect, useState } from "react";
 import { LoginModel } from "../Models/LoginModel";
+import axios from "axios";
+import { LoginResponse } from "../Models/LoginResponse";
+import config from '../Config.json'
 
 export interface AuthenticationData{
     username?: string;
@@ -10,37 +13,63 @@ export interface AuthenticationData{
 
 export const AuthenticationContext = createContext<AuthenticationData>({isLoggedIn : false, logout: ()=>{}});
 
-// function jwtToUsername(jwt?: string): string {
-//     if (jwt){
-//         const payload = jwt.split('.')[1];
-//         const asJson = atob(payload);
-//         const asObj = JSON.parse(asJson);
+function jwtToUsername(jwt?: string): string {
+    if (jwt){
+        const payload = jwt.split('.')[1];
+        const asJson = atob(payload);
+        const asObj = JSON.parse(asJson);
 
-//         return asObj['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'];
-//     }
-//     else{
-//         return '';
-//     }
-// }
+        return asObj['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'];
+    }
+    else{
+        return '';
+    }
+}
 
 export const AuthenticationProvider = ({children}: PropsWithChildren) => {
-    // const JWT_KEY = 'JWT_KEY';
+    const JWT_KEY = 'JWT_KEY';
 
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [username, setUsername] = useState('');
+    const [jwt, setJwt] = useState(sessionStorage.getItem(JWT_KEY) ?? undefined);
+    const [isLoggedIn, setIsLoggedIn] = useState(!!jwt);
+    const [username, setUsername] = useState(jwtToUsername(jwt));
+
+    useEffect(()=>{
+        if(isLoggedIn){
+            axios.interceptors.request.use(req =>{
+                const isToServer = req.url?.startsWith(config.serverUrl);
+
+                if (isToServer){
+                    req.headers['Authorization'] = `Bearer ${jwt}`;
+                }
+
+                return req;
+            });
+        }
+        else{
+            axios.interceptors.request.clear();
+        }    
+    }, [isLoggedIn,jwt]);
 
     const login = async (model: LoginModel):Promise<Date> => {
-        console.log('AuthenticationService.login called');
+        const response = await axios.post<LoginResponse>(`${config.serverUrl}/api/authentication/login`, model);
 
+        if (response.status !== 200){
+            throw new Error('Login failed.');
+        }
+
+        sessionStorage.setItem(JWT_KEY, response.data.jwtToken);
+        setJwt(response.data.jwtToken);
         setIsLoggedIn(true);
-        setUsername('Dummy User');
+        setUsername(jwtToUsername(response.data.jwtToken));
 
-        return new Date('2023-08-01T14:40:22Z');
+        return new Date(response.data.expiration);
     }
 
     const logout = () =>{
         setIsLoggedIn(false);
         setUsername('');
+        setJwt(undefined);
+        sessionStorage.removeItem(JWT_KEY);
     }
 
     return (
--- src/Services/ReviewRepository.ts
+++ src/Services/ReviewRepository.ts
@@ -1,16 +1,17 @@
 
 import { BookReview } from "../Models/BookReview";
 import config from '../Config.json'
+import axios from "axios";
 
 class ReviewRepository{
     private async getReviewsCommon(path: string):Promise<BookReview[]> {
-        const response = await fetch(`${config.serverUrl}/api/${path}`);
+        const response = await axios.get<BookReview[]>(`${config.serverUrl}/api/${path}`);
 
         if (response.status !== 200){
             throw new Error('Failed to retrieve data.')
         }
 
-        return await response.json();   
+        return response.data;   
     }
 
     async getReviews(): Promise<BookReview[]> {
